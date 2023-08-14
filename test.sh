0083eb08d209f2eb7bb00b68c0a0e2e47b004cdc
probonopd <probonopd@users.noreply.github.com>
2023-08-14 23:47:00 +0200

    Exit with exit code

--- test.sh
+++ test.sh
@@ -24,15 +24,14 @@ else
     echo "One or more attrutil commands failed."
 fi
 
-# Compare the written value with the read value
-expected_value="Test attribute value"
-if [ "$read_value" = "$expected_value" ]; then
-    echo "Attribute value matches the expected value."
-else
-    echo "Attribute value does not match the expected value."
-fi
-
 # Clean up: Delete the test file
 rm "$test_file"
 
-echo "Test complete."
+# Compare the exit codes
+if [ "$write_exit_code" -eq 0 ] && [ "$list_exit_code" -eq 0 ] && [ "$read_exit_code" -eq 0 ]; then
+    echo "All attrutil commands succeeded."
+    exit 0
+else
+    echo "One or more attrutil commands failed."
+    exit 1
+fi
